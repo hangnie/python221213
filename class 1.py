de33668bbac72f2d85fac261fc00a6554909224d
hangnie <hangnie@gmail.com>
2022-12-14 09:36:55 +0900

    클래스 연습2

---
+++ class 1.py
@@ -0,0 +1,14 @@
+# class 1.py
+# 클래스 정의
+class person:
+    #초기화 매서드
+     def __init__(self):
+        self.name = "default name"
+    #인스턴스 메서드
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+#인스턴스 생성
+p1 = Person()
+#메서드 호출
+p1.print()
