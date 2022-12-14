46a9ee0afb21e18b8015b57a10f499646b14bbe5
hangnie <hangnie@gmail.com>
2022-12-14 11:57:00 +0900

    클래스 상속2

--- class 2.py
+++ class 2.py
@@ -1,18 +1,17 @@
 # class 1.py
 # 클래스 정의
 class Person:
-    #초기화 매서드
+    # 클래스 멤버 변수(일종의 공통 데이터로 사용)
+    num_person = 0
     def __init__(self):
         self.name = "default name"
+        Person.num_person += 1
     #인스턴스 메서드
     def print(self):
         print("My name is {0}".format(self.name))
 
 #인스턴스 생성
 p1 = Person()
-p1 = Person()
-p1.name = "전우치"
-
-#메서드 호출
-p1 = print()
-p2 = print()
+p2 = Person()
+p3 = Person()
+print ("인스턴스 갯수:{0}".format(Person.num_person))
